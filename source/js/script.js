8925066c9bc79275006b80f2b78a660a85c30335
SalabayNatalia <salabay.natalia89@gmail.com>
2022-07-10 02:23:46 +0300

    Продолжаю работу над проектом.

--- source/js/script.js
+++ source/js/script.js
@@ -70,3 +70,18 @@ window.addEventListener('keydown', function (evt) {
     }
   }
 });
+
+new Swiper('.swiper-container', {
+
+  direction: 'vertical',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+   nextEl: ".swiper-button-next",
+   prevEl: ".swiper-button-prev",
+  },
+});
